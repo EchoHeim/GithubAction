565c3e5a8b229decf1ca56c45b7f5dd7a3e06a88
SourceTree-Lodge <shilong.native@gmail.com>
2022-08-17 03:25:51 +0800

    selenium Collated

--- selenium/Futu/SeedFarm.py
+++ selenium/Futu/SeedFarm.py
@@ -1,18 +1,20 @@
+# -*- coding: UTF-8 -*-
+
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
-import platform
-import traceback
-
-import os, sys, time
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
+
+import platform
+import traceback
+import os, sys, time
 
 username = sys.argv[1] # 登录账号
 password = sys.argv[2] # 登录密码
+
 
 # 富途牛牛 种子农场 登录地址
 website='https://passport.futunn.com/?target=https%3A%2F%2Fseed.futunn.com%2F%3Flang%3Dzh-cn%26panel%3Dcultureroom#login'
@@ -23,14 +25,13 @@ chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-dev-shm-usage')
 
+print("==== 检测环境 ====")
 if platform.system() == 'Windows':
     print('\nCurrent Operating System: ==== Windows ====\n')
-
     browser = webdriver.Chrome('chromedriver.exe')
 
 elif platform.system() == 'Linux':
     print('\nCurrent Operating System: ==== Linux ====\n')
-
     chromedriver = "/usr/bin/chromedriver"
     os.environ["webdriver.chrome.driver"] = chromedriver
     browser = webdriver.Chrome(chrome_options=chrome_options,executable_path=chromedriver)
@@ -46,27 +47,26 @@ browser.find_element(By.XPATH,'//*[@id="loginFormWrapper"]/form/input[4]').click
 time.sleep(2)
 print(browser.title)
 
-browser.find_element(By.XPATH,'//*[@class="waterCanvas"]').click()          # 自己浇水
+print("==== 给自己浇水 ====")
+browser.find_element(By.XPATH,'//*[@class="waterCanvas"]').click()          
 time.sleep(1)
 
-browser.find_element(By.XPATH,'/html/body/div[1]/div/div[8]/ul/li[3]/a').click()        # 好友列表
+print("==== 进入好友列表 ====")
+browser.find_element(By.XPATH,'/html/body/div[1]/div/div[8]/ul/li[3]/a').click()
 time.sleep(1)
-browser.find_element(By.XPATH,'//*[@class="filter-op"]/span').click()                   # 筛选 可施肥
+print("==== 筛选 (可施肥) ====")
+browser.find_element(By.XPATH,'//*[@class="filter-op"]/span').click()
 time.sleep(2)
-print(browser.title)
-
-def seed_friend():
-    browser.find_element(By.XPATH,'//*[@class="can_fert icon_friends-fert"]').click()       # 选中一位好友
-    time.sleep(1)
-    browser.find_element(By.XPATH,'//*[@class="opIcon icon_fert"]').click()         # 浇水
-    time.sleep(1)
-    browser.find_element(By.XPATH,'//*[@class="back-home"]/span').click()           # 返回好友列表
-    time.sleep(1)
 
 for i in range(1,20):
     try:
-        seed_friend
-        print ("friend: %d" % i)
+        browser.find_element(By.XPATH,'//*[@class="can_fert icon_friends-fert"]').click()   # 选中一位好友
+        time.sleep(1)
+        browser.find_element(By.XPATH,'//*[@class="opIcon icon_fert"]').click()         # 浇水
+        time.sleep(1)
+        browser.find_element(By.XPATH,'//*[@class="back-home"]/span').click()           # 返回好友列表
+        time.sleep(1)
+        print ("==== 施肥成功 %d 好友 ====" % i)
     except:
         traceback.print_exc()
     finally:
